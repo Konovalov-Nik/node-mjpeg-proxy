7d3b4957dec9497f148b1e55a6d4b7dd94c8cf0d
Nikita Konovalov <konovalovnikita@gmail.com>
2018-05-22 14:01:18 +0400

    Install express
    log start

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node
 
 WORKDIR /home/node
 
+RUN npm install express
 RUN npm install mjpeg-proxy
 
 COPY app.js ./
--- app.js
+++ app.js
@@ -2,5 +2,7 @@ var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
 var app = express();
 
+console.log("starting");
+
 app.get('/main.mjpg', new MjpegProxy('http://192.168.2.222:8080/?action=stream').proxyRequest);
 app.listen(8080);
